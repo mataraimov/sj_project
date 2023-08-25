61fdf6f648834e39ccaace592e93d89e715b0639
aksss7 <aksss7.kg@gmail.com>
2023-08-25 22:35:43 +0600

    first commit

---
+++ src/components/layout/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from "./Sidebar.module.css";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <h1 className={s.title}>Kairat </h1>
+    </div>
+  );
+};
+
+export default Sidebar;
