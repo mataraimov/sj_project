684851ff70481f8d7448340809a91733e3c4dc25
mataraimov <minuradil@gmail.com>
2023-12-10 20:57:15 +0600

    fix

--- src/components/utils/config.js
+++ src/components/utils/config.js
@@ -1,3 +1,3 @@
-export const DOMAIN = 'sj.kg';
+export const DOMAIN = 'http://139.59.132.105';
 export const DEBUG_MODE = window.location.hostname === 'localhost';
 export const API_URL = DEBUG_MODE ? 'http://139.59.132.105' : `https://${DOMAIN}`;
